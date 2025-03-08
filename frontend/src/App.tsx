01a528491dee17ba0c01f49756b8f63ab434fd1d
mikolaj5 <mikypicky@gmail.com>
2025-03-08 20:22:53 +0000

    trying out react and creating a card component

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import './App.css';
+import Card from "./Components/CardList/CardList";
+import CardList from './Components/CardList/CardList';
+
+function App() {
+  return (
+    <div className="App">
+      <CardList />
+    </div>
+  );
+}
+
+export default App;
