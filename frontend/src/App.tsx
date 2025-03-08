251e6e3e2ecc18414189ea84c27e2ace81125b83
mikolaj5 <mikypicky@gmail.com>
2025-03-08 22:30:31 +0000

    api key and call function

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,10 +2,12 @@ import * as React from 'react'
 import './App.css';
 import Card from "./Components/CardList/CardList";
 import CardList from './Components/CardList/CardList';
+import Search from './Components/Search/Search';
 
 function App() {
   return (
     <div className="App">
+      <Search />
       <CardList />
     </div>
   );
