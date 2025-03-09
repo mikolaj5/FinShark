7db17a8343a4355053fa750dd2b0ae50b64a330d
mikolaj5 <mikypicky@gmail.com>
2025-03-09 11:00:58 +0000

    Moving event handling away from search component into app.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,11 +3,21 @@ import './App.css';
 import Card from "./Components/CardList/CardList";
 import CardList from './Components/CardList/CardList';
 import Search from './Components/Search/Search';
+import { SyntheticEvent, useState } from 'react';
 
 function App() {
+  const [search, setSearch] = useState<string>("");
+    
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+      console.log(e);
+  }
+  const onClick = (e: SyntheticEvent) => {
+      console.log(e);
+  };
   return (
     <div className="App">
-      <Search />
+      <Search onClick = {onClick} search = {search} handleChange = {handleChange} />
       <CardList />
     </div>
   );
--- frontend/src/Components/Search/Search.tsx
+++ frontend/src/Components/Search/Search.tsx
@@ -1,17 +1,13 @@
-import React, { JSX, useState } from 'react'
+import React, { ChangeEvent, FormEvent, JSX, SyntheticEvent, useState } from 'react'
 
-type Props = {}
+interface Props {
+  onClick: (e: SyntheticEvent) => void;
+  search: string | undefined;
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
 
-const Search: React.FC<Props> = (props: Props): JSX.Element => {
-    const [search, setSearch] = useState<string>("");
-    
-    const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setSearch(e.target.value);
-        console.log(e);
-    }
-    const onClick = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
-        console.log(e);
-    };
+}
+
+const Search: React.FC<Props> = ({onClick, search, handleChange}: Props): JSX.Element => {
   return (
     <div>
         <input value = {search} onChange= {(e) => handleChange(e)}></input>
